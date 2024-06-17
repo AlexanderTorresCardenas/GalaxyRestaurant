8814b01da58d0ad31918616cd07e115c0bacb6d7
Alexander Torres Cardenas <alextorresc5@hotmail.com>
2024-06-16 23:04:46 -0500

    Restaurant FrondEnd

---
+++ Restaurant.Server/Controllers/ClientesController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Restaurant.Entities;
+using Restaurant.Repositories.Interfaces;
+using Restaurant.Shared;
+
+namespace Restaurant.Server.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ClientesController : ControllerBase
+{
+    private readonly IClienteRepository _repository;
+    public ClientesController(IClienteRepository clienteRepository)
+    {
+        _repository = clienteRepository;
+    }
+
+
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        return Ok(await _repository.ListAsync());
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post(ClienteDTO request)
+    {
+
+        var cliente = new Cliente
+        {
+            Nombres = request.Nombres,
+            Apellidos = request.Apellidos,
+            FechaCreacion = (DateTime)request.FechaNacimiento,
+            TipoDocumento = request.TipoDocumento,
+            NumeroDocumento = request.NumeroDocumento,
+            NumeroTelefonoMovil = request.NumeroTelefonoMovil,
+            CorreoElectronico = request.CorreoElectronico
+        };
+        await _repository.AddAsync(cliente);
+        return Ok();
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<IActionResult> Get(int id)
+    {
+        var cliente = await _repository.FindAsync(id);
+        if (cliente is null)
+        {
+            return NotFound();
+        }
+        return Ok(cliente);
+    }
+}
---
+++ Restaurant.Shared/ClienteDTO.cs
@@ -0,0 +1,27 @@
+﻿using Restaurant.Entities.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace Restaurant.Shared;
+
+public class ClienteDTO
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = Constantes.CampoRequerido)]
+    [Display(Name = "Nombres")]
+    public string Nombres { get; set; } = default!;
+
+    [Required(ErrorMessage = Constantes.CampoRequerido)]
+    [Display(Name = "Apellidos")]
+    public string Apellidos { get; set; } = default!;
+    public DateTime? FechaNacimiento { get; set; }
+    public TipoDocumento TipoDocumento { get; set; } = default!;
+
+    [Required(ErrorMessage = Constantes.CampoRequerido)]
+    [Display(Name = "Numero de documento")]
+    public string NumeroDocumento { get; set; } = default!;
+    public string? NumeroTelefonoMovil { get; set; }
+
+    [EmailAddress]
+    public string? CorreoElectronico { get; set; }
+}
